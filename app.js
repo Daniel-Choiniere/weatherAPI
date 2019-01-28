a2a0e270e2311dc0d2e05b9c9bdc362bbac679fd
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-01-28 16:17:53 +0000

    dohhh realized my bug wasn't a CORS issue with  darkSky API but just a stupid typo..

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ window.addEventListener("load", () => {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
             console.log(position);
-            long = position.coords.longtitude;
+            long = position.coords.longitude;
             lat = position.coords.latitude;
             
             const proxy = 'https://cors-anywhere.herokuapp.com/';
             // api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}
-            const api = `${proxy}https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&APPID=7efc20ad68027c60035208cabfd5ea02`;
-            // const api = `https://api.darksky.net/forecast/15882ae30429d0f0d653f9f3bfb1fb7f/${lat},${long}`;
+            // const api = `${proxy}https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&APPID=7efc20ad68027c60035208cabfd5ea02`;
+            const api = `${proxy}https://api.darksky.net/forecast/15882ae30429d0f0d653f9f3bfb1fb7f/${lat},${long}`;
             
         fetch(api)
             .then(response => {
