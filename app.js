546ac18ab4521e2b13dc4a24673589e82a86da78
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-01-28 16:53:27 +0000

    added helpful comments

--- app.js
+++ app.js
@@ -3,23 +3,29 @@ window.addEventListener("load", () => {
     let long;
     let lat;
     
+    // uses built in navigator.geolocation method to get users current longtitude and latitude
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
-            console.log(position);
+            // console.log(position);
+            
+            // saves the users long and lat into variables
             long = position.coords.longitude;
             lat = position.coords.latitude;
             
+            // use a proxy to route through to bypass CORS issue with darkSky API
             const proxy = 'https://cors-anywhere.herokuapp.com/';
-            // api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}
-            // const api = `${proxy}https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&APPID=7efc20ad68027c60035208cabfd5ea02`;
             const api = `${proxy}https://api.darksky.net/forecast/15882ae30429d0f0d653f9f3bfb1fb7f/${lat},${long}`;
             
+            // retrieve (fetch) the data from the darkSky API response
         fetch(api)
             .then(response => {
+                // return the data in JSON format
                 return response.json();
             })
             .then(data => {
+                // log the data to the console behind the scenes
                 console.log(data);
+                const {temprature, summary} = data.currently;giut 
             });
         });
     }
